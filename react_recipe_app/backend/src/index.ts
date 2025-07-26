89448d31fa914ac7572fc6a3b3b3d6a8ddac1b73
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-07-26 02:07:20 +0200

    Creating backend folder , installing dependecies and index.ts setup

---
+++ react_recipe_app/backend/src/index.ts
@@ -0,0 +1,18 @@
+import express,{Request, Response} from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+
+dotenv.config()//load env variables
+
+const app = express()//create express app
+
+app.use(cors())//cross origin resource sharing
+app.use(express.json())//parse json
+
+
+app.get("/", async (req:Request, res:Response) => {
+    res.json({"message":"Hello World!"})
+})
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))   
