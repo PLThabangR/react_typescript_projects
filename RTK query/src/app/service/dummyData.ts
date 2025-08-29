1300bbd41d465a22861109f7e4dfe6f1759dd09f
PLThabangR <rakgoropothabang@gmail.com>
2025-08-29 16:44:34 +0200

    rtk query setup completed with all logic

--- RTK query/src/app/service/dummyData.ts
+++ RTK query/src/app/service/dummyData.ts
@@ -2,13 +2,23 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 // create an instance of query and pass an object to it
-export const dummyApi = createApi({
-    reducerPath: 'product',//name of method
+export const ProductApi = createApi({
+    reducerPath: 'products',//name of method
     //url where we fetch our data
-    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com'}),
+    //Here we define our endpoints
     endpoints: (builder) => ({
-        getPosts: builder.query({
-            query: () => '/posts'
-        })  
-    })
-});
+        //Get all the products from the api
+        //we use the builder.query method because we not changing the data
+        //if we want to change the data we use builder.mutation  
+        getAllProducts: builder.query({
+            //we gonna be sending request to this specific url /products
+            query: () => '/porducts'
+        }),  // end of get produdcts endpoint
+
+    })//end of endpoints builder
+
+});//end of createApi
+//we export getAllProducts method from the dummpyApi
+// and use it in our component as a hook 
+export const {useGetAllProductsQuery} = ProductApi
--- RTK query/src/app/store.ts
+++ RTK query/src/app/store.ts
@@ -1,10 +1,27 @@
 import {configureStore} from '@reduxjs/toolkit'
+//listener is helpful for caching the data
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { ProductApi } from './service/dummyData'
 
 
+// initialize the store
 export const store = configureStore({
 
 //folder to hold our reducers
 reducer:{
+    /*in the rtk query we use this syntax while defining our reducers
+    and the react redux toolkit we use this
+    productApi: ProductApi.reducer
+    */
+[ProductApi.reducerPath]: ProductApi.reducer
+},
 
-}
-})
+//middleware
+// Allows ProductApi to handle asynchronous API requests.
+middleware: (getDefaultMiddleware) =>
+    
+     getDefaultMiddleware().concat(ProductApi.middleware),
+})//end of store
+
+//setup the listeners for data caching
+setupListeners(store.dispatch)
