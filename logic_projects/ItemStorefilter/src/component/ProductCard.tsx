702860eaa7c917380282ef1d466dbc54c29e7a54
PLThabangR <rakgoropothabang@gmail.com>
2025-09-05 01:58:44 +0200

    Creating card

--- logic_projects/ItemStorefilter/src/component/ProductCard.tsx
+++ logic_projects/ItemStorefilter/src/component/ProductCard.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import { Link } from 'react-router'
 
 interface ProductCardProps {
-    
+    id: string,
+    title: string,
+    thumbnail: string,
+    price: number
+
 }
-const ProductCard = () => {
+const ProductCard = ({id,title,thumbnail,price}:ProductCardProps) => {
   return (
-    <div>ProductCard</div>
+    <>
+      <div className='border p-4 rounded-2xl'>
+        <Link to={`/product/${id}`}>
+        <img src={thumbnail} alt={title} className='w-full h-32 object-cover' />
+        <h1>{title}</h1>
+        <h2>${price}</h2>
+        </Link>
+        </div>  
+
+    </>
   )
 }
 
