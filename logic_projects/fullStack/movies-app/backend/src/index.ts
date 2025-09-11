7a4a3e4ffc4847c58323387b476cc7200a93bdb0
PLThabangR <rakgoropothabang@gmail.com>
2025-09-12 01:44:42 +0200

    Connected the db and fixed the importing issue

--- logic_projects/fullStack/movies-app/backend/src/index.ts
+++ logic_projects/fullStack/movies-app/backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello world");
+import express from 'express';
+import type { Express } from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import cookieParser from 'cookie-parser'
+import path from 'path'
+
+
+
+
+
+//Instace of express
+const app : Express = express()
+//Dotenv
+dotenv.config()
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(process.env.PORT,()=>
+
+console.log(`Server running on port ${process.env.PORT}`))
+
+connectDB()
+
