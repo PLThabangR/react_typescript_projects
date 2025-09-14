e26d8bfb9703261354ddfb8ecbe35aa3c8692c93
PLThabangR <rakgoropothabang@gmail.com>
2025-09-14 22:03:01 +0200

    working flex

--- portfolio/src/Home.tsx
+++ portfolio/src/Home.tsx
@@ -5,23 +5,74 @@ const Home = () => {
     //#5140D7 -primary color
   return (
     <>
+    {/* header */}
 <div className="m-5">
 <section>
   <div className="flex flex-col justify-center items-center m-2 p-2">
 <h1 className="text-5xl font-bold">Software Developer</h1>  
-<p className="text-1xl"> with hands-on experience in developing web applications using React and Node.js</p>
+<p className="text-2xl mt-2 p-2"> with hands-on experience in developing web applications using Javascript</p>
+
+<div className="flex flex-col gap-2 m-2 p-2 items-center md:flex-row xl:flex-row ">
+  <p className="text-1xl">linkedin</p>
+<p className="text-1xl">Github</p>
+</div>
 
 </div>
 </section>
-<section  className="m-5 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"> 
-  <h1 className="text-2xl font-bold text-center">Experience</h1> 
-  <div className="flex flex-col justify-center items-center md:flex-row xl:flex-row">
+{/* projects */}
+<section>
+  <h1 className="text-2xl font-bold text-center">Projects</h1> 
+<div className="flex flex-col gap-2 m-2 p-2  items-center md:flex-row xl:flex-row md:flex-wrap xl:flex-wrap">
+<div  className="w-1/3 grow-4  p-5 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"> 
+   <h3 className="text-2xl font-bold text-center">App 1</h3> 
+  <div className="w-full flex flex-col  justify-center mt-2 md:flex-row xl:flex-row ">
      
     <p className="text-1xl">with hands-on experience in developing web applications using React and Node.js</p>
-    <img src="https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="w-1/4 bg-cover " alt="company logo" />
+    <img src="https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="w-1/4 bg-cover ml-5 " alt="company logo" />
 
 
   </div>
+</div>
+<div  className="w-1/2 p-5 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"> 
+   <h3 className="text-2xl font-bold text-center">App 1</h3> 
+  <div className="flex flex-col  justify-center mt-2 md:flex-row xl:flex-row ">
+     
+    <p className="text-1xl">with hands-on experience in developing web applications using React and Node.js</p>
+    <img src="https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="w-1/4 bg-cover ml-5 " alt="company logo" />
+
+
+  </div>
+</div>
+
+<div  className="w-1/3 grow-4 p-5 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"> 
+   <h3 className="text-2xl font-bold text-center">App 1</h3> 
+  <div className="flex flex-col  justify-center mt-2 md:flex-row xl:flex-row ">
+     
+    <p className="text-1xl">with hands-on experience in developing web applications using React and Node.js</p>
+    <img src="https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="w-1/4 bg-cover ml-5 " alt="company logo" />
+
+
+  </div>
+</div>
+<div  className="w-1/2 p-5 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"> 
+   <h3 className="text-2xl font-bold text-center">App 1</h3> 
+  <div className="flex flex-col  justify-center mt-2 md:flex-row xl:flex-row ">
+     
+    <p className="text-1xl">with hands-on experience in developing web applications using React and Node.js</p>
+    <img src="https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" className="w-1/4 bg-cover ml-5 " alt="company logo" />
+
+
+  </div>
+</div>
+
+
+</div>
+
+
+
+
+
+
 </section>
 
 
