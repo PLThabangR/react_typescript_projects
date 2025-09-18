fc55953e88b8ea2c64a1361d4ab27e300248d54e
PLThabangR <rakgoropothabang@gmail.com>
2025-09-19 00:51:38 +0200

    Big O notation comp[eted

--- dataStructures/bigOnotation/bigNotation.js
+++ dataStructures/bigOnotation/bigNotation.js
@@ -26,10 +26,38 @@ console.log(results);
 //this function is constant
 //the rime to find an item will remain constant
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [1, 2, 3, 4]
 //if you know the index of the item
 const  findItem=(array,index) => array[index]; 
 
 
 console.log(findItem(numbers,0));
 
+// ************Part 3*************** */
+//O(n^2)
+//this function is logarithmic
+//this dunction time complexity will increase coz is a double
+function findPairs(){
+    //O(n^2) this means nested loops
+    for(let i=0;i<numbers.length;i++){
+        for(let j=i+1;j<numbers.length;j++){
+            console.log("Pair:",numbers[i],numbers[j]);
+        }
+    }
+
+    //o(n)
+    for (let i = 0; i < numbers.length; i++) {
+        console.log("Number:", numbers[i]);
+    }
+
+    // this will result in a time complexity of O(n^2+n)
+}
+
+
+findPairs(numbers);
+
+
+// O(log n)
+/*
+algorithim that grows  logarithmic with size input
+*/ 
