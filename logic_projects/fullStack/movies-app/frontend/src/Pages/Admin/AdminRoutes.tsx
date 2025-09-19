9b404c4327572784ff2555a54f9a43ef3dfec83c
PLThabangR <rakgoropothabang@gmail.com>
2025-09-19 20:16:12 +0200

    Admin component logic added

--- logic_projects/fullStack/movies-app/frontend/src/Pages/Admin/AdminRoutes.tsx
+++ logic_projects/fullStack/movies-app/frontend/src/Pages/Admin/AdminRoutes.tsx
@@ -1,8 +1,19 @@
+import { useSelector } from 'react-redux'
+import { Outlet, Navigate } from 'react-router-dom'
 
 const AdminRoutes = () => {
-  return (
-    <div>AdminRoutes</div>
-  )
+    //get user from store
+    const {userInfo} = useSelector((state:any) => state.auth)
+    //check if user is available and that if user is admin 
+    if(userInfo && userInfo.isAdmin){
+        //if user is admin return outlet relevent page if is admin
+        return <Outlet />
+    }else{
+        //if user is not admin redirect to login
+        return <Navigate to="/login" />
+    }
+
+
 }
 
 export default AdminRoutes
