f0aec0f2a9aff5f82db066acf6d46fa410538000
PLThabangR <rakgoropothabang@gmail.com>
2025-09-19 20:00:49 +0200

    creating end the crud endpoints in the rtk qurey

--- logic_projects/fullStack/movies-app/frontend/src/app/api/genre.ts
+++ logic_projects/fullStack/movies-app/frontend/src/app/api/genre.ts
@@ -2,6 +2,8 @@ import { apiSlice } from "./apiSlice";
 import { GENRE_URL } from "../constants";
 
 
+
+
 //we use injectEndpoints to create endpoints
 export const genresApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -9,15 +11,35 @@ export const genresApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: GENRE_URL,
             }),
-            providesTags: ["Genre"],
+           
         }),//end of get all gengres
         createGenre: builder.mutation({
-            query: (genre) => ({
+            query: (newGenre) => ({
                 url: GENRE_URL,
                 method: "POST",
-                body: genre,
+                body: newGenre,
             }),
-            invalidatesTags: ["Genre"],
-        })
-    }),
-});
+        }),//end of create genre
+        updateGenre: builder.mutation({
+            query: (data) => ({
+                url: `${GENRE_URL}/${data.id}`,
+                method: "PUT",
+                body: data,
+            }),
+        }),//end of update genre
+
+            deleteGenre: builder.mutation({
+                query: (id) => ({
+                    url: `${GENRE_URL}/${id}`,
+                    method: "DELETE",
+                }),
+            }),//end of delete genre
+            getGenreById: builder.query({
+                query: (id) => ({
+                    url: `${GENRE_URL}/${id}`,
+                }),
+            }),//end of get genre by id
+
+          
+        })//end of endpoints
+    })//end of injectEndpoints
