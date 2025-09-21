131b31011ecd94c559710bcd7f4726f45832b133
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-09-21 20:03:37 +0200

    creatiing routes logic

--- logic_projects/fullStack/movies-app/backend/src/routes/moviesRoutes.ts
+++ logic_projects/fullStack/movies-app/backend/src/routes/moviesRoutes.ts
@@ -3,10 +3,18 @@ import express from "express";
 //import middlewares
 import { authenticateUser ,authorizationAdmin} from "../middleware/authMiddleware";
 import { createMovie, deleteMovie, getAllMovies, getMovieById, updateMovie } from "../controller/movieController";
-import { checkId } from "../middleware/checkId";
+import  checkId  from "../middleware/checkId";
 
 
 const router = express.Router();
+
+//public routes
+router.get("/",getAllMovies);
+router.get("/:id",getMovieById)
+//restricted routes
+router.post("/",authenticateUser,authorizationAdmin,createMovie);
+router.put("/:id",authenticateUser,authorizationAdmin,checkId,updateMovie);
+router.delete("/:id",authenticateUser,authorizationAdmin,checkId,deleteMovie)
 
 
 
