82851042503f4904396ee6f3d9aed85505c1bb16
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-09-22 00:19:10 +0200

    Fixed the request problem

--- logic_projects/fullStack/movies-app/backend/src/controller/moviesController.ts
+++ logic_projects/fullStack/movies-app/backend/src/controller/moviesController.ts
@@ -10,7 +10,7 @@ import MovieModel from "../model/Movies";
 import GenreModel from "../model/Genre";
 
 
-export const createMovie = async (res: Response, req: Request) => {
+export const createMovie = async (req: Request,res: Response) => {
     const { title, image, year, details, genre, cast } = req.body;
 
    
@@ -67,7 +67,16 @@ export const createMovie = async (res: Response, req: Request) => {
 }
 
 
-export const getAllMovies = async (res: Response, req: Request) => {}
+export const getAllMovies = async (res: Response, req: Request) => {
+    try{
+            //all movies
+        const movies = await MovieModel.find({}).populate('genre');
+        //send response
+        res.status(200).json(movies);
+    }catch(error:any){
+        return res.status(500).json({message: (error as Error).message});
+    }
+}
 
 
 export const getMovieById = async (res: Response, req: Request) => {}
