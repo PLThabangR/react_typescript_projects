d2a88a5612bd13115f72afb959b7047886a09993
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-09-24 01:35:39 +0200

    palindrome solution done

---
+++ dataStructures/customArrays/challenges/philondrom.js
@@ -0,0 +1,9 @@
+
+const username = "John Doe";
+
+const usernameToArray = username.split(""); 
+
+const reverseArray= (str)=> str.split("").reverse().join("") == str;
+
+
+console.log(reverseArray("abba"));
