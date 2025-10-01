3704ef7eb9ebccf32e5c3260a5f6e358bd57bb7e
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-01 05:14:34 +0200

    Desrtructuring and display

--- react_demo/typescript_basics/src/components/Display.tsx
+++ react_demo/typescript_basics/src/components/Display.tsx
@@ -11,12 +11,14 @@ const Display = ({name,age,isStudent,_id}: UserType) => {
     console.log(name,age,isStudent)
   return (
     <>
+    <h1>User Details</h1>
  <div>
+
     <p>{_id}</p>
      <h1>{name}</h1>
     <p>{age}</p>
     <p>{isStudent}</p>
-    
+
    </div>
 
 
