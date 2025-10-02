1f76ac4b8655f1a6951d9f397023900cf27aa48d
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-02 05:13:19 +0200

    Java file setup

---
+++ Java/bascics/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    /**
+     * This is the main method of the class.
+     * It prints "Hello World" to the standard error stream.
+     * @param args The command line arguments passed to the program
+     */
+    public static void main(String[] args) {
+        System.err.println("Hello World");
+    }
+}
