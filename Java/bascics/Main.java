6625b741ff69179b98e607f50ad3b1eccec29e28
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-02 05:43:54 +0200

    Operators in java

--- Java/bascics/Main.java
+++ Java/bascics/Main.java
@@ -7,8 +7,52 @@ public class Main {
      * @param args The command line arguments passed to the program
      */
     public static void main(String[] args) {
-     
+      // Call the void function
+        arithmeticOperators();
+        // Call the void function
+        relationalOperators();
 
-        
+      
+    }
+
+    //We use staic method so we do not initialize the call
+     static void arithmeticOperators() {
+        int a = 10;
+        int b = 5;
+        System.out.println("Arithmetic Operators");
+        int sum = a + b;
+        System.out.println(sum);
+        // subtraction
+        int sub = a - b;
+        System.out.println(sub);
+        // multiplication
+        int mul = a * b;
+        System.out.println(mul);
+        // division
+        int div = a / b;
+        System.out.println(div);
+        // remainder
+        int mod = a % b;
+        System.out.println(mod);
+    }
+   static void  relationalOperators() {
+        int a = 10;
+        int b = 5;
+        System.out.println("Relational Operators");
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+
+    static void logicalOperators() {
+        int a = 10;
+        int b = 5;
+        System.out.println("Logical Operators");
+        System.out.println(a > b && a < b);
+        System.out.println(a > b || a < b);
+        System.out.println(!(a > b));
     }
 }
---
+++ Java/bascics/operators/Main.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class Main {
+
+  /**
+   * This is the main method of the class.
+   * It prints "Hello World" to the standard error stream.
+   * 
+   * @param args The command line arguments passed to the program
+   */
+  public static void main(String[] args) {
+
+    int a = 10;
+    int b = 5;
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+  }
+}
