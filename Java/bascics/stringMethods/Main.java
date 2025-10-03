3d9e46075866220cc1de7d4f8d4dce5449ae9f93
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-03 06:41:14 +0200

    String merthods in Java

--- Java/bascics/stringMethods/Main.java
+++ Java/bascics/stringMethods/Main.java
@@ -8,14 +8,30 @@ public class Main {
      * @param args The command line arguments passed to the program
      */
     public static void main(String[] args) {
-    //Creating String i Java
-    String names[] = {"Jack", "John", "Jill"};
+        String text= "Adventure";
 
-    //Accessing String
-    System.out.println(names[0]);
-    System.out.println(names[1]);
-    System.out.println(names[2]);
+        //length
+        int length = text.length();
+        System.out.println("Length: " + length);
 
+        //charAt
+        char firstChar = text.charAt(2);
+        System.out.println("First Character: " + firstChar);
+
+        String substring = text.substring(1,4);
+        System.out.println("Substring: " + substring);
+  
+        String text1 = "Adventure";
+        boolean contains = text1.contains("vent");
+        System.out.println("Contains 'vent': " + contains);
+
+        String text2 = text1.replace("e", "a");
+        System.out.println("Replace a with e ': " + text2);
+
+
+        for(int i = 0; i < 2; i++) {
+            System.out.println("Iteration " + i);
+        }
     }
 
 }
