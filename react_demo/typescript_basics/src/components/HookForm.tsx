167909e461de7d65ab7accc6d97406bfd3ba23b4
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-03 08:14:42 +0200

    Mastering react hook form library

--- react_demo/typescript_basics/src/components/HookForm.tsx
+++ react_demo/typescript_basics/src/components/HookForm.tsx
@@ -19,17 +19,29 @@ const HookForm = () => {
 
 //Creating the on Submit functiopn
 const onsubmit :SubmitHandler<FormData> = (data) => {
+   
     console.log(data)
 }
   return (
     <>
 <form onSubmit={handleSubmit(onsubmit)}>
-    <input type="text" placeholder='Name' {...register('name')} />
-    <input type="email" placeholder='Email' {...register('email')} />
-    <input type="password" placeholder='Password' {...register('password')} />
-    <button type='submit' disabled={isSubmitting}>Submit</button>
+    <input type="text" placeholder='Name' {...register('name',{required:"Name is required"})} />
+    {errors.name && <p  style={{color:"red"}}>{errors.name.message}</p>}
+    <input type="text" placeholder='Email' {...register('email',{required:"Email is required",
+         pattern:{value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message:"Invalid email address"}
+    })} />
+     {errors.email && <p style={{color:"red"}}>{errors.email.message}</p>}
+    <input type="password" placeholder='Password' {...register('password',
+    {required:"Password is required",
+    minLength:{value:6, message:"Password must be at least 6 characters"}}
+    )} />
+     {errors.password && <p style={{color:"red"}}>{errors.password.message}</p>}
+
+    <button type='submit' disabled={isSubmitting}>{isSubmitting?"Submitting...":"Submit"}</button>
 
 </form>
+
+
     </>
   )
 }
