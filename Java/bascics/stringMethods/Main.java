3a89a703da5dc37d2c2906bde5346c6998f6bd17
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-03 05:40:13 +0200

    Creating Array in Java

---
+++ Java/bascics/stringMethods/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    /**
+     * This is the main method of the class.
+     * It prints "Hello World" to the standard error stream.
+     * 
+     * @param args The command line arguments passed to the program
+     */
+    public static void main(String[] args) {
+    //Creating String i Java
+    String names[] = {"Jack", "John", "Jill"};
+
+    //Accessing String
+    System.out.println(names[0]);
+    System.out.println(names[1]);
+    System.out.println(names[2]);
+
+    }
+
+}
+
