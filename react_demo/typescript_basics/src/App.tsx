f296d48544d971e8b0c800cd25d1b4da32969e92
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-03 08:48:59 +0200

    USing the use hook to fetch data

--- react_demo/typescript_basics/src/App.tsx
+++ react_demo/typescript_basics/src/App.tsx
@@ -12,6 +12,9 @@ function App() {
     <>
         {/* <User/> */}
           {/* <Form/> */}
+           <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
           <HookForm/>
     </>
   )
---
+++ react_demo/typescript_basics/src/components/React_19/Use.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Use = () => {
+  //USe hook let you to read and to load resources asynchronously such as promises and context
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Use
