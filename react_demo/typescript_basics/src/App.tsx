bb5bc888593b5e810b854126cbd242bf6fd03845
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-03 07:34:19 +0200

    Using use ref to get curerent values

--- react_demo/typescript_basics/src/App.tsx
+++ react_demo/typescript_basics/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import { User } from './components/User'
+import Form from './components/Form'
 
 function App() {
  // const [count, setCount] = useState(0)
 
   return (
     <>
-        <User/>
+        {/* <User/> */}
+          <Form/>
     </>
   )
 }
--- react_demo/typescript_basics/src/components/Form.tsx
+++ react_demo/typescript_basics/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 interface User{
     name: string,
@@ -8,8 +8,41 @@ interface User{
 
 const Form = () => {
     const [userInfo, setUserInfo] = useState<User>({name: '', email: '', password: ''});
+    const name =useRef<HTMLInputElement>(null);
+       const email =useRef<HTMLInputElement>(null);
+          const password =useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+   
+        //Get curerent value of inputs and tell compiler that they are not null
+         const nameValue : string = name.current!.value;
+        const emailValue : string = email.current!.value;
+        const passwordValue : string = password.current!.value;
+
+        setUserInfo({name: nameValue, email: emailValue, password: passwordValue});
+      
+    }
+
   return (
-    <div>Form</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Name' ref={name}   />
+            <input type="email" placeholder='Email' ref={email}  />
+            <input type="password" placeholder='Password' ref={password}  />
+            <button type='submit'>Submit</button>   
+
+
+        </form>
+
+        <section>
+                <h1>Name: {userInfo.name}</h1>
+                <h1>Email: {userInfo.email}</h1>
+                <h1>Password: {userInfo.password}</h1>
+        </section>
+
+    </div>
   )
 }
 
