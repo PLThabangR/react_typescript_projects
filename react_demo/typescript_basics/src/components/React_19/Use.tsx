a0b4ade42701c6a75870deecc3cc47564e483d8c
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-04 09:55:12 +0200

    creating the fetch data function

--- react_demo/typescript_basics/src/components/React_19/Use.tsx
+++ react_demo/typescript_basics/src/components/React_19/Use.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { use } from 'react'
+
+//function to fetch data
+const fetchData = async() => {
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  
+  return response.json()
+}
 
 const Use = () => {
   //USe hook let you to read and to load resources asynchronously such as promises and context
-  
+const data = use(fetchData())  
 
   return (
     <div>
