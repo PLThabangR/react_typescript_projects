623b1a89f0cfe489862af1bcad3fed65bb088fa5
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-04 10:25:32 +0200

    installing react icons

--- logic_projects/blogs/src/components/Navigation.tsx
+++ logic_projects/blogs/src/components/Navigation.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
 
 const Navigation = () => {
   return (
     <nav className='flex justify-between  items-center border-2 text-black p-4 border-gray-200'>
         <div className='flex iteems-center border-2 rounded-full px-2 max-w-md ml-[5rem]'>
-
+                <FaSearch/>
         </div>
 
 
