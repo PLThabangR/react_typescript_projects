31cbaa0960e17f716402cc31ffc4c3bb5566bcff
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-05 01:24:46 +0200

    Installing redux for binding

--- logic_projects/blogs/src/app/store.ts
+++ logic_projects/blogs/src/app/store.ts
@@ -4,9 +4,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { blogSlice } from './features/blogSlice'
 
-
+//Configure store
 export const store = configureStore({
   reducer: {
   blog: blogSlice.reducer,  
   },
 })
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
