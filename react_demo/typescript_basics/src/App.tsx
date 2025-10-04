186233ce13df2ac0b2c7a3d189089971141cb3b6
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-04 10:04:23 +0200

    Wont worki in react yet

--- react_demo/typescript_basics/src/App.tsx
+++ react_demo/typescript_basics/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { Suspense } from 'react'
 
 import './App.css'
 import { User } from './components/User'
 import Form from './components/Form'
 import HookForm from './components/HookForm'
+import UseHook from './components/React_19/UseHook'
 
 function App() {
  // const [count, setCount] = useState(0)
@@ -15,7 +16,11 @@ function App() {
            <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
-          <HookForm/>
+          {/* <HookForm/> */}
+          <Suspense>
+<UseHook/>
+          </Suspense>
+          
     </>
   )
 }
--- react_demo/typescript_basics/src/components/React_19/UseHook.tsx
+++ react_demo/typescript_basics/src/components/React_19/UseHook.tsx
@@ -1,22 +1,25 @@
-import React, { use } from 'react'
+"use server"
+import { use } from 'react'
 
 //function to fetch data
 const fetchData = async() => {
 
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  
+
   return response.json()
 }
 
-const Use = () => {
+const UseHook = () => {
+  
   //USe hook let you to read and to load resources asynchronously such as promises and context
 const data = use(fetchData())  
 
   return (
     <div>
-
+      <h2>This is th use</h2>
+      <h1>{data.title}</h1>
     </div>
   )
 }
 
-export default Use
+export default UseHook
