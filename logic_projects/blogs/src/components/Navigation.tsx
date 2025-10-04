282d76384129c051c82880e54ff309ec665afe4c
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-04 10:20:49 +0200

    creating the navigation

--- logic_projects/blogs/src/components/Navigation.tsx
+++ logic_projects/blogs/src/components/Navigation.tsx
@@ -1,1 +1,16 @@
-rafce
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className='flex justify-between  items-center border-2 text-black p-4 border-gray-200'>
+        <div className='flex iteems-center border-2 rounded-full px-2 max-w-md ml-[5rem]'>
+
+        </div>
+
+
+    </nav>
+  )
+}
+
+export default Navigation
+
