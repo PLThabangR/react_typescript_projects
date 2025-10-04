2a7b08974f1c52ab9ccf8c5def859cd22f38ef04
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2025-10-04 10:53:15 +0200

    Created button and new componet

---
+++ logic_projects/blogs/src/components/TrendzList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrendzList = () => {
+  return (
+    <div>TrendzList</div>
+  )
+}
+
+export default TrendzList
--- logic_projects/blogs/src/components/USerCard.tsx
+++ logic_projects/blogs/src/components/USerCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaUserCircle } from 'react-icons/fa'
 
 interface PeopleToFollow {
     name:string
@@ -6,8 +7,12 @@ interface PeopleToFollow {
 }
 const USerCard = ({name,following}:PeopleToFollow) => {
   return (
-    <div>
-        
+    <div className='flex justify-between items-center'>
+        <section className='flex items-center '>
+        <FaUserCircle className='text-3xl text-gray-500'/>
+        <span className='ml-2'>{name}</span>
+        </section>
+        <button className='bg-black text-white rounded-full px-4 py-2'>{following?"Following":"Follow"}</button>
     </div>
   )
 }
